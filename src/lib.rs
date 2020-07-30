08aa12a44712c9b9d7f949fb5ced40be92ee135e
ggguille <g.granado.g@gmail.com>
2020-07-30 14:13:14 +0200

    Writing a Failing Test

--- src/lib.rs
+++ src/lib.rs
@@ -10,14 +10,14 @@ pub struct Config {
 
 impl Config {
   pub fn new(args: &[String]) -> Result<Config, &'static str> {
-      if args.len() < 3 {
-          return Err("not enough arguments");
-      }
+    if args.len() < 3 {
+      return Err("not enough arguments");
+    }
 
-      let query = args[1].clone();
-      let filename = args[2].clone();
+    let query = args[1].clone();
+    let filename = args[2].clone();
 
-      Ok(Config { query, filename })
+    Ok(Config { query, filename })
   }
 }
 
@@ -27,4 +27,24 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
   println!("With text:\n{}", contents);
 
   Ok(())
+}
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+  vec![]
+}
+
+#[cfg(test)]
+mod test {
+  use super::*;
+
+  #[test]
+  fn one_result() {
+    let query = "duct";
+    let contents = "\
+      Rust:
+      safe, fast, productive.
+      Pick three.";
+    
+    assert_eq!(vec!["safe, fast, productive."], search(query, contents))
+  }
 }
