33ede12ebc76fe3cebf0980cae21d53170df36bc
ggguille <g.granado.g@gmail.com>
2020-07-30 15:28:30 +0200

    Writing a Failing Test for the Case-Insensitive search Function

--- src/lib.rs
+++ src/lib.rs
@@ -46,7 +46,7 @@ mod test {
   use super::*;
 
   #[test]
-  fn one_result() {
+  fn case_sensitive() {
     let query = "duct";
     let contents = "\
       Rust:
@@ -54,5 +54,16 @@ mod test {
       Pick three.";
     
     assert_eq!(vec!["safe, fast, productive."], search(query, contents))
+  }
+
+  #[test]
+  fn case_insensitive() {
+    let query = "rUsT";
+    let contents = "\
+      Rust:
+      safe, fast, productive.
+      Pick three.";
+    
+    assert_eq!(vec!["Rust:", "Trust me."], search(query, contents))
   }
 }
